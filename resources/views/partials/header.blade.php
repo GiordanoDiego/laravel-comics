3c0c4dff4d8207d0207f0334b5ce2a357739fc15
GiordanoDiego <diegomgiordano96@gmail.com>
2024-02-24 17:11:54 +0100

    header completed, start main

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,85 @@
 
 <header>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                header
+    <div class="top-header">
+        <div class="container">
+            <div class="row">
+                <div class="col text-end ">
+                    DC POWERVISA®  |  ADDITIONAL DC SITES 
+                </div>
             </div>
         </div>
     </div>
+    <div class="bottom-header">
+        <div class="container">
+            <div class="row pt-1 pb-1">
+                <div class="col-1 ">
+                   <img src="./img/dc-logo.png" alt="">
+                </div>
+                <div class="col">
+                   <ul>
+                        <li>
+                            <a href="">
+                                CHARACTERS
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                               COMICS 
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                              MOVIES 
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                              TV 
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                              GAMES 
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                COLLECTIBLES 
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                VIDEO 
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                FANS 
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                NEWS 
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                SHOP 
+                            </a>
+                        </li>
+                   </ul>
+                </div>
+                <div class="col-3 d-flex align-items-center ">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Search..." aria-label="Recipient's username" aria-describedby="button-addon2">
+                        <button class="btn btn-outline-secondary" type="button" id="button-addon2">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </button>
+                      </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 </header>
--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -1,18 +1,109 @@
 
-{{-- esempio di uso sezione, definisco una sessione --}}
-@section('main-content')
-
-  <h2> Qui c'è il main</h2>
-@endsection
-
-
 <main>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                @yield('main-content') 
-                {{-- uso la sessione --}}
+    <section class="section1">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+
+                </div>
             </div>
         </div>
-    </div>
+    </section>
+
+
+    <section class="section2">
+        <div class="container">
+            <div class=" position-relative ">
+                <button>
+                    CURRENT SERIES
+                </button>
+            </div>
+            <div class="row">
+                <div class="col">
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="section3">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    ciao
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col button-container-load">
+                    <button>
+                        LOAD MORE
+                    </button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="section4">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section4">
+        <div class="container">
+            <div class="row">
+                <div class="col d-flex  justify-content-between ">
+                    <div class=" d-flex  align-items-center ">
+                        <div class="ico_container">
+                            <img src="/img/buy-comics-digital-comics.png" alt="">
+                        </div>
+                        <div>
+                            DIGITAL COMICS
+                        </div>
+                    </div>
+                    <div class=" d-flex  align-items-center ">
+                        <div class="ico_container">
+                            <img src="/img/buy-comics-merchandise.png" alt="">
+                        </div>
+                        <div>
+                            DC MERCHANDISE
+                        </div>
+                    </div>
+                    <div class=" d-flex  align-items-center ">
+                        <div class="ico_container">
+                            <img src="/img/buy-comics-subscriptions.png" alt="">
+                        </div>
+                        <div>
+                            SUBSCRIPTION
+                        </div>
+                    </div>
+                    <div class=" d-flex  align-items-center ">
+                        <div class="ico_container">
+                            <img src="/img/buy-comics-shop-locator.png" alt="">
+                        </div>
+                        <div>
+                            COMIC SHOP LOCATOR
+                        </div>
+                    </div>
+                    <div class=" d-flex  align-items-center ">
+                        <div class="ico_container">
+                            <img src="/img/buy-dc-power-visa.svg" alt="">
+                        </div>
+                        <div>
+                            DC POWER VISA
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
 </main>
